1011d5c712debb91fd0b187c9023b16c32c0760a
pratz1988 <prathyusha.jaddu@gmail.com>
2019-12-20 01:28:56 -0500

    Note from parent

--- daycare_portal_client/src/components/NoteFromParents.js
+++ daycare_portal_client/src/components/NoteFromParents.js
@@ -1,23 +1,51 @@
 
 import _ from 'lodash'
 import React, { Component } from 'react'
+import axios from "axios";
 import { Button, Image, List, Transition } from 'semantic-ui-react'
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-const users = ['plz apply coconut oil for every 1 hr on his elbows', 'dont give milk to Shourya today', 'dont give to Dimpi milk toda', 'Carnky Today morning', 'Plz apply aquaphor', 'Sohan has diaperrash plz apply oil','Plz use big Jacket for Dimpi','Use Aquaphor for Sid']
+let base_url = "https://daycare-portal.herokuapp.com";
 
+if (process.env.NODE_ENV === "development") {
+  base_url = "http://localhost:3000";
+}
+
+const users = ['plz apply coconut oil for every 1 hr on his elbows for vivan', 'dont give milk to Shourya today', 'dont give to Dimpi milk toda', 'Sohan is Carnky Today morning', 'Plz apply aquaphor for Ishi', 'Dheeru has diaperrash plz apply oil','Plz use big Jacket for Dimpi','Use Aquaphor for Sid']
+const imgs = ['https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTj5iCJgOgctuuVACE9BtuocPEzKEaOaBO6Mpc6wxjQyvTMlMau&s','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRx4HWmym_MtNbfavFqHumIag0L3Iq-MW3QWvCADoSP-CICgEhAjw&s','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQN2h-FznabevY6dYjYoC2nhszaU4N1qEIQyh85cJVW3Ns5DmG&s','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRjXM5aLx8KmTMIbFaRDSos6BzmUd2yBMbUVeCwtuCqXMHI8rw&s','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRA64pBFPI52SUcCRGfZDSyvg04XHKt27A8-tc5NjqtAh8mjK5q&s','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLSO-UKfjAiO9DNNjntRNCS5i5PRgi32D9HmulGdEE-4D4k4xpbg&s',''];
 export default class TransitionExampleGroup extends Component {
 
     constructor(props) {
         super(props)
         this.state = { 
             items: users.slice(0, 6) ,
-            allChildrenData: []
+            images: imgs,
+            allChildrenNames: [],
+            notes:  []
         }
     //    this.state.allChildrenData = this.props.allChildrenData
-       console.log(this.state.allChildrenData);
+    this.getAllChildrenNmaes = this.getAllChildrenNmaes.bind(this);
     }
   
+    componentDidMount() {
+        this.getAllChildrenNmaes();
+      }
+
+    //Get all Children Names & All notes from parents
+  async getAllChildrenNmaes() {
+    const response = await axios(`${base_url}/children`);
+    let childrenData = response.data;
+    this.setState({
+      allChildrenNames: childrenData
+    });
+    this.state.allChildrenNames.map((note,index)  =>  {
+        this.state.notes  =  note.noteFromParent
+    })
+    
+    // this.props.getNoteFromParents(childrenData);
+    console.log("all child data..", this.state.allChildrenNames);
+    console.log("all child data..", this.state.allChildrenNames[0].childName);
+  }
 
   handleAdd = () =>
     this.setState((prevState) => ({
@@ -28,11 +56,12 @@ export default class TransitionExampleGroup extends Component {
     this.setState((prevState) => ({ items: prevState.items.slice(0, -1) }))
 
   render() {
-    const { items } = this.state
+    const { items,images } = this.state
 
     return (
       <div >
            <div class="ui pointing secondary menu ">
+               
             <Link to="/">
                 <a class=" item">Home</a>
                 </Link>
@@ -43,8 +72,10 @@ export default class TransitionExampleGroup extends Component {
                 <div class="right menu">
                 <Link to="/"> <a class="item">logout</a></Link>
                 </div>
+                
             </div>
-        <div className="noteMarginLeft">
+        <div className="noteMarginLeft ">
+        <div class="ui four  column segment">
         <Button.Group>
           <Button
             disabled={items.length === 0}
@@ -65,15 +96,16 @@ export default class TransitionExampleGroup extends Component {
           size='huge'
           verticalAlign='middle'
         >
-          {items.map((item) => (
+          {items.map((item,index) => (
             <List.Item key={item}>
-              <Image avatar src={`/images/avatar/small/${item}.jpg`} />
+              <Image avatar src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTj5iCJgOgctuuVACE9BtuocPEzKEaOaBO6Mpc6wxjQyvTMlMau&s" />
               {/* <span avatar>name</span> */} {""}{""}{""}{""}
               <List.Content header={_.startCase(item)} />
             </List.Item>
           ))}
         </Transition.Group>
         </div>
+      </div>
       </div>
     )
   }
